c79ad699f1bfd5d31689eb6728579370f8712c92
lazypolarbear <ezvaseg@gmail.com>
2022-07-19 11:07:15 -0500

    NavBar colored

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
+import { Banner } from '../components/Banner';
 import { NavBar } from '../components/NavBar';
 
 export const MainLayout = () => {
    return (
-      <div
-         className="fixed sm:static sm:h-full bg-[url('../assets/img/banner-bg.png')] "
-         style={{}}
-      >
+      <div className="fixed sm:static sm:h-full bg-stone-800" style={{}}>
          <div className="relative w-full ">
             <NavBar />
-            <div className="pb-14 sm:pb-16 overflow-y-scroll sm:overflow-hidden h-screen sm:h-full ">
+            <div className="pb-14 sm:pb-16 overflow-y-scroll sm:overflow-hidden h-screen sm:h-full bg-gradient-to-b from-stone-800 to-purple-900">
+               <Banner />
                <div className="container">
                   Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                   Ullam commodi veniam natus quam dolorem dolore non laboriosam
@@ -73,7 +72,49 @@ export const MainLayout = () => {
                   amet consectetur adipisicing elit. Ullam commodi veniam natus
                   quam dolorem dolore non laboriosam architecto sapiente eius
                   autem mollitia omnis neque odit praesentium, iure aliquid
-                  aliquam quis!
+                  aliquam quis!esentium, iure aliquid aliquam quis! Lorem, ipsum
+                  dolor sit amet consectetur adipisicing elit. Ullam commodi
+                  veniam natus quam dolorem dolore non laboriosam architecto
+                  sapiente eius autem mollitia omnis neque odit praesentium,
+                  iure aliquid aliquam quis!esentium, iure aliquid aliquam quis!
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Ullam commodi veniam natus quam dolorem dolore non laboriosam
+                  architecto sapiente eius autem mollitia omnis neque odit
+                  praesentium, iure aliquid aliquam quis!esentium, iure aliquid
+                  aliquam quis! Lorem, ipsum dolor sit amet consectetur
+                  adipisicing elit. Ullam commodi veniam natus quam dolorem
+                  dolore non laboriosam architecto sapiente eius autem mollitia
+                  omnis neque odit praesentium, iure aliquid aliquam
+                  quis!esentium, iure aliquid aliquam quis! Lorem, ipsum dolor
+                  sit amet consectetur adipisicing elit. Ullam commodi veniam
+                  natus quam dolorem dolore non laboriosam architecto sapiente
+                  eius autem mollitia omnis neque odit praesentium, iure aliquid
+                  aliquam quis!esentium, iure aliquid aliquam quis! Lorem, ipsum
+                  dolor sit amet consectetur adipisicing elit. Ullam commodi
+                  veniam natus quam dolorem dolore non laboriosam architecto
+                  sapiente eius autem mollitia omnis neque odit praesentium,
+                  iure aliquid aliquam quis!esentium, iure aliquid aliquam quis!
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Ullam commodi veniam natus quam dolorem dolore non laboriosam
+                  architecto sapiente eius autem mollitia omnis neque odit
+                  praesentium, iure aliquid aliquam quis!esentium, iure aliquid
+                  aliquam quis! Lorem, ipsum dolor sit amet consectetur
+                  adipisicing elit. Ullam commodi veniam natus quam dolorem
+                  dolore non laboriosam architecto sapiente eius autem mollitia
+                  omnis neque odit praesentium, iure aliquid aliquam
+                  quis!esentium, iure aliquid aliquam quis! Lorem, ipsum dolor
+                  sit amet consectetur adipisicing elit. Ullam commodi veniam
+                  natus quam dolorem dolore non laboriosam architecto sapiente
+                  eius autem mollitia omnis neque odit praesentium, iure aliquid
+                  aliquam quis!esentium, iure aliquid aliquam quis! Lorem, ipsum
+                  dolor sit amet consectetur adipisicing elit. Ullam commodi
+                  veniam natus quam dolorem dolore non laboriosam architecto
+                  sapiente eius autem mollitia omnis neque odit praesentium,
+                  iure aliquid aliquam quis!esentium, iure aliquid aliquam quis!
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Ullam commodi veniam natus quam dolorem dolore non laboriosam
+                  architecto sapiente eius autem mollitia omnis neque odit
+                  praesentium, iure aliquid aliquam quis!
                </div>
             </div>
          </div>
