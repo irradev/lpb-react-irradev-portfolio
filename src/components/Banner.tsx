d3d39fea9c78cc28379cd1721242ae98285fe5db
lazypolarbear <ezvaseg@gmail.com>
2022-07-22 00:19:20 -0500

    Banner - changed to DynamicText tool

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,9 +1,18 @@
-import React, { FC, useEffect, useMemo, useState } from 'react';
+import React from 'react';
 import TrackVisibility from 'react-on-screen';
+import { DynamicText } from './tools/DynamicText';
 import graphic from '../assets/img/graphic_banner.png';
 import 'animate.css';
 
 export const Banner = () => {
+   const toRotate = [
+      'Tec. Ingeniería de Software',
+      'Desarrollador Web',
+      'Especialista en React',
+      'Investigador UX',
+      'Amante del Pixel Art <3',
+   ];
+
    return (
       <section className="banner " id="home">
          <div className="container flex justify-center items-center h-screen ">
@@ -26,7 +35,10 @@ export const Banner = () => {
                                  Soy Israel Vázquez
                               </span>
                               <div className="flex justify-center md:justify-start items-center w-full text-2xl h-12 sm:text-3xl mxs:text-xl">
-                                 <DynamicText isVisible={isVisible} />{' '}
+                                 <DynamicText
+                                    isVisible={isVisible}
+                                    toRotate={toRotate}
+                                 />{' '}
                               </div>
                            </h1>
                            <p className="text-secondary text-lg sm:text-xl pb-0 pt-4  mxs:pt-3">
@@ -48,81 +60,5 @@ export const Banner = () => {
             </div>
          </div>
       </section>
-   );
-};
-
-interface DynamicTextProps {
-   isVisible: boolean;
-}
-
-const DynamicText: FC<DynamicTextProps> = ({ isVisible }) => {
-   const delayTyping = 100;
-   const delayBeforeDelete = 1300;
-
-   const [time, setTime] = useState<number>(0);
-   const [miliSeconds, setMiliSeconds] = useState(delayTyping);
-   const [isStopInterval, setIsStopInterval] = useState(false);
-   const [isDeleting, setIsDeleting] = useState(false);
-   const [index, setIndex] = useState(0);
-   const [text, setText] = useState('');
-
-   const toRotate = [
-      'Tec. Ingeniería de Software',
-      'Desarrollador Web',
-      'Especialista en React',
-      'Investigador UX',
-      'Amante del Pixel Art <3',
-   ];
-
-   useEffect(() => {
-      let ticker = setInterval(() => {
-         setTime((time) => time + 1);
-      }, miliSeconds);
-
-      if (!isVisible) {
-         clearInterval(ticker);
-      } else if (isStopInterval) {
-         clearInterval(ticker);
-         setTimeout(() => {
-            setIsStopInterval(false);
-         }, delayBeforeDelete);
-      }
-
-      return () => {
-         clearInterval(ticker);
-      };
-   }, [isVisible, isStopInterval, miliSeconds]);
-
-   useEffect(() => {
-      let fullText = toRotate[index];
-      let updatedText = isDeleting
-         ? fullText.substring(0, text.length - 1)
-         : fullText.substring(0, text.length + 1);
-
-      setText(updatedText);
-
-      if (!isDeleting && updatedText === fullText) {
-         setIsDeleting(true);
-         setIsStopInterval(true);
-         setMiliSeconds((prevmiliSeconds) => prevmiliSeconds / 2);
-      } else if (isDeleting && updatedText === '') {
-         setMiliSeconds(delayTyping);
-         setIsDeleting(false);
-         if (index === toRotate.length - 1) {
-            setIndex(0);
-         } else {
-            setIndex(index + 1);
-         }
-      }
-   }, [time]);
-
-   return useMemo(
-      () => (
-         <div className="wrap">
-            <span>{text}</span>
-            <span className="intermittent-animation">|</span>
-         </div>
-      ),
-      [text]
    );
 };
