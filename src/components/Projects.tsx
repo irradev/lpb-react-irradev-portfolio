6433de046c150d55f13adbdf0b75a3b87fbf81ae
lazypolarbear <ezvaseg@gmail.com>
2022-07-22 01:46:33 -0500

    Using  TabButton component instead inline button

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,16 +1,15 @@
 import React, { useEffect, useMemo, useState } from 'react';
-import { SectionCard } from './ui/SectionCard';
-import { projects as dataProjects, IProject, TProject } from '../data/projects';
-import { CardProject } from './CardProject';
-
-import 'animate.css';
 import TrackVisibility from 'react-on-screen';
+import { SectionCard } from './ui/SectionCard';
+import { CardProject } from './CardProject';
+import { TabButton } from './TabButton';
+
+import { projects as dataProjects, IProject, TProject } from '../data/projects';
+import 'animate.css';
 
 export const Projects = () => {
    const tabs: TProject[] = ['Frontend', 'Backend', 'Fullstack'];
-
    const [tabSelected, setTabSelected] = useState<TProject>('Frontend');
-
    const [projects, setProjects] = useState<IProject[]>([]);
 
    useEffect(() => {
@@ -39,31 +38,18 @@ export const Projects = () => {
                   <div className=" flex flex-col-reverse sm:flex-col h-full w-full pb-3 sm:pb-6">
                      <TrackVisibility>
                         {({ isVisible }) => (
-                           <div className="flex-shrink-0 flex justify-around sm:justify-center items-center sm:gap-10 h-16 my-2">
+                           <div className="flex-shrink-0 flex justify-around sm:justify-center items-center  h-16 my-2">
                               <>
                                  {tabs.map((tab, index) => (
-                                    <button
-                                       key={`button_tab_${index + 1}`}
-                                       className={`
-                                          bg-teal-700 
-                                          text-sm font-bold mxs:text-xs
-                                          border border-teal-500 
-                                          px-3 py-2 mxs:px-1 msx:py-1
-                                          uppercase tracking-wide 
-                                          rounded-md shadow-md shadow-teal-700
-                                          ${
-                                             isVisible
-                                                ? 'animate__animated animate__bounceIn'
-                                                : ''
-                                          }
-                                    `}
+                                    <TabButton
+                                       key={`button_tab_` + index}
+                                       isVisible={isVisible}
+                                       title={tab}
+                                       index={index}
+                                       totalTabs={tabs.length - 1}
+                                       isActive={tabSelected === tab}
                                        onClick={() => setTabSelected(tab)}
-                                       style={{
-                                          animationDelay: `${index / 6}s`,
-                                       }}
-                                    >
-                                       {tab}
-                                    </button>
+                                    />
                                  ))}
                               </>
                            </div>
