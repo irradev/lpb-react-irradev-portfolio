04134605e6d0b53b8f2c463004b2c842688753a8
lazypolarbear <ezvaseg@gmail.com>
2022-07-22 03:18:02 -0500

    Added CardSkill component

---
+++ src/components/CardSkill.tsx
@@ -0,0 +1,28 @@
+import React, { FC, ReactElement } from 'react';
+import TrackVisibility from 'react-on-screen';
+
+interface CardSkillProps {
+   name: string;
+   icon: ReactElement;
+   index: number;
+}
+
+export const CardSkill: FC<CardSkillProps> = ({ name, icon, index }) => {
+   return (
+      <TrackVisibility once={true}>
+         {({ isVisible }) => (
+            <div
+               className={`flex flex-col justify-center items-center hover:-translate-y-4 transition-all duration-300 ease-in-out cursor-default m-2
+                ${isVisible ? 'animate__animated animate__fadeInLeft' : ''}
+               `}
+               style={{
+                  animationDelay: `${index / 8}s`,
+               }}
+            >
+               <div className="text-6xl ">{icon}</div>
+               <span className="text-lg">{name}</span>
+            </div>
+         )}
+      </TrackVisibility>
+   );
+};
