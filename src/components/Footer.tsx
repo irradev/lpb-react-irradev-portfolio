27b62ea1f11a8d2e526adfb80028d9db901cd8c3
lazypolarbear <ezvaseg@gmail.com>
2022-07-28 05:04:14 -0500

    Added Footer Section

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+import 'animate.css';
+
+export const Footer = () => {
+   return (
+      <div className="flex justify-center items-center w-full py-5 text-xl font-medium gap-4 cursor-default text-shadow-xs hover:scale-125 transition-transform duration-700 ease-in-out">
+         <a
+            className="hover:text-teal-300 transition-colors duration-500 ease-in-out"
+            href="https://twitter.com/irradev"
+            target="_blank"
+         >
+            @irraDev
+         </a>
+         <span>-</span>
+         <span className=""> {new Date().getFullYear()}</span>
+      </div>
+   );
+};
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
 import { NavBar } from '../components/NavBar';
 import { Banner } from '../components/Banner';
 import { Skills } from '../components/Skills';
 import { Projects } from '../components/Projects';
 import { Contact } from '../components/Contact';
+import { Footer } from '../components/Footer';
 
 export const MainLayout = () => {
    return (
@@ -14,6 +14,7 @@ export const MainLayout = () => {
             <Skills />
             <Projects />
             <Contact />
+            <Footer />
          </div>
       </div>
    );
