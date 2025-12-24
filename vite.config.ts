64f175e3a341803f2564266bdcc77032fca4b878
irradev <israeldevcorel@gmail.com>
2025-12-24 04:24:01 -0600

    feat: Implement conditional Vite base path for GitHub Pages deployment and update project configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/lpb-react-irradev-portfolio/',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'GITHUB_PAGES')
+  return {
+    plugins: [react()],
+    base: env.GITHUB_PAGES ? '/lpb-react-irradev-portfolio/' : '/',
+  }
 })
