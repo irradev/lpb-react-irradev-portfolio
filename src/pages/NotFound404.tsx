a7f40356470b1f10e34b8c5e046324c272b6b67a
irradev <israeldevcorel@gmail.com>
2025-12-24 05:42:08 -0600

    refactor: move animation delay from 404 page button to parent container to fix CSS transition

--- src/pages/NotFound404.tsx
+++ src/pages/NotFound404.tsx
@@ -8,9 +8,12 @@ export const NotFound404 = () => {
             <h1 className="text-9xl font-bold text-rose-600 animate__animated animate__fadeIn">404</h1>
             <p className="text-4xl text-secondary animate__animated animate__fadeInRight">Page not found</p>
             <p className="text-lg text-mutted my-10 text-center animate__animated animate__fadeIn" style={{ animationDelay: `0.4s` }}>La página que buscas puede haber sido eliminada, su nombre ha cambiado o está temporalmente no disponible.</p>
-            <div className="flex justify-center items-center w-full animate__animated animate__fadeInUp animate__slow">
+            <div className="flex justify-center items-center w-full animate__animated animate__fadeInUp animate__slow" style={{ animationDelay: `1.3s` }}>
 
-                <button className="flex justify-center items-center gap-2 border border-gray-200 hover:border-gray-400 hover:scale-105 text-lg font-bold p-2 mt-2 w-full max-w-sm uppercase tracking-wide rounded-md shadow-lg text-shadow-xs transition-all duration-300 ease-out" style={{ animationDelay: `1.3s` }} onClick={() => navigate('/')}>
+                <button
+                    onClick={() => navigate('/')}
+                    className="flex justify-center items-center gap-2 border border-gray-200 hover:border-gray-400 hover:scale-105 text-lg font-bold p-2 mt-2 w-full max-w-sm uppercase tracking-wide rounded-md shadow-lg text-shadow-xs transition-all duration-300 ease-out"
+                >
                     <AiOutlineHome fontSize={22} />
                     Ir a la página principal</button>
             </div>
