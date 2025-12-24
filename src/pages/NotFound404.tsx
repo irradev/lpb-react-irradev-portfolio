bb785f360702a49af226b3905ff2f880ade2a81e
irradev <israeldevcorel@gmail.com>
2025-12-24 05:31:58 -0600

    feat: animate 404 page elements and update home navigation button icon and text.

--- src/pages/NotFound404.tsx
+++ src/pages/NotFound404.tsx
@@ -1,16 +1,19 @@
 import { useNavigate } from "react-router";
-import { BiArrowBack } from "react-icons/bi";
+import { AiOutlineHome } from 'react-icons/ai';
 
 export const NotFound404 = () => {
     const navigate = useNavigate();
     return (
         <div className="flex-grow flex flex-col justify-center items-center h-full max-w-lg mx-auto ">
-            <h1 className="text-9xl font-bold text-rose-600">404</h1>
-            <p className="text-4xl text-secondary">Page not found</p>
-            <p className="text-lg text-mutted my-10 text-center">La página que buscas puede haber sido eliminada, su nombre ha cambiado o está temporalmente no disponible.</p>
-            <button className="flex justify-center items-center gap-2  border border-gray-200 hover:border-gray-400 hover:scale-105 text-lg font-bold p-2 mt-2 w-full max-w-sm uppercase tracking-wide rounded-md shadow-lg text-shadow-xs transition-all duration-300 ease-out" onClick={() => navigate('/')}>
-                <BiArrowBack fontSize={22} />
-                Volver a la página principal</button>
+            <h1 className="text-9xl font-bold text-rose-600 animate__animated animate__fadeIn">404</h1>
+            <p className="text-4xl text-secondary animate__animated animate__fadeInRight">Page not found</p>
+            <p className="text-lg text-mutted my-10 text-center animate__animated animate__fadeIn" style={{ animationDelay: `0.4s` }}>La página que buscas puede haber sido eliminada, su nombre ha cambiado o está temporalmente no disponible.</p>
+            <div className="flex justify-center items-center w-full animate__animated animate__fadeInUp animate__slow">
+
+                <button className="flex justify-center items-center gap-2 border border-gray-200 hover:border-gray-400 hover:scale-105 text-lg font-bold p-2 mt-2 w-full max-w-sm uppercase tracking-wide rounded-md shadow-lg text-shadow-xs transition-all duration-300 ease-out" style={{ animationDelay: `1.3s` }} onClick={() => navigate('/')}>
+                    <AiOutlineHome fontSize={22} />
+                    Ir a la página principal</button>
+            </div>
         </div>
     );
 };
